18d938dff85b79d2dd0cb1f50d0263bb169c7566
HDragos24 <hc.dragos@gmail.com>
2022-11-02 15:07:11 +0200

    DB config

--- Migrations/20221102130210_PublishingDate.Designer.cs
+++ Migrations/20221102130210_PublishingDate.Designer.cs
@@ -4,6 +4,7 @@ using Hritcu_DragosCristian_Lab2.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Hritcu_DragosCristian_Lab2.Migrations
 {
     [DbContext(typeof(Hritcu_DragosCristian_Lab2Context))]
-    partial class Hritcu_DragosCristian_Lab2ContextModelSnapshot : ModelSnapshot
+    [Migration("20221102130210_PublishingDate")]
+    partial class PublishingDate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/Book.cs
+++ Models/Book.cs
@@ -1,10 +1,22 @@
-﻿namespace Hritcu_DragosCristian_Lab2.Models
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Xml.Linq;
+
+namespace Hritcu_DragosCristian_Lab2.Models
 {
     public class Book
     {
         public int ID { get; set; }
+
+        [Display(Name = "Book Title")]
         public string Title { get; set; }
         public string Author { get; set; }
+
+        [Column(TypeName = "decimal(6, 2)")]
         public decimal Price { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime PublishingDate { get; set; }
     }
 }
